ad7dfe3422c08d57989ecc2fb7748ad73021fc99
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-01-22 00:46:02 +0200

    working with edit issue

--- __tests__/edit-file.test.js
+++ __tests__/edit-file.test.js
@@ -5,14 +5,13 @@ jest.mock('fs');
 const reader = require('../lib/edit-file');
 
 describe('file reader module as callback',()=>{
+    let file =`${__dirname}/data/person.json`;
     it('when given a bad file, return an error',()=>{
-        let file =`${__dirname}/data/person.json`;
         reader.readerFile(file,(err,data)=>{
             expect(err).toBe(undefined);
         })
     })
     it('when given the same file, return content',()=>{
-        let file =`${__dirname}/data/person.json`;
         reader.readerFile(file,(err,data)=>{
             expect(typeof data).toBe('string');
         })
@@ -22,8 +21,8 @@ describe('file write module as callback',()=>{
     it('when given the same file, return content',()=>{
         let file =`${__dirname}/data/person.json`;
         reader.editFile(file,(err,data)=>{
-            console.log(data)
-            expect(typeof data.firstName).toBe('string');
+            // console.log(data)
+            expect(typeof data).toBe('string');
         })
     })
 })

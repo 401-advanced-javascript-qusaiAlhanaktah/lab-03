0ee142ddcb811ea3dcb3325696097d484c457eec
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-01-26 23:14:43 +0200

    update some data

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 'use strict';
-
-const fs = require('fs');
-const util = require('util');
-
-const reader = require('./lib/edit-file')
-
-let file =`${__dirname}/data/person.json`;
-
-reader.readerFile(file, (err, data) => {
-    if (err) { throw err; }
-    console.log('Callback from module', data);
-  });
